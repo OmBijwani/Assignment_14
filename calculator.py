390a9170155edddc6e538dc9985ba687ec2ab4f8
OmBijwani <ombijwani06@gmail.com>
2026-06-04 10:05:55 +0530

    Base Code

---
+++ calculator.py
@@ -0,0 +1,24 @@
+def add(a, b):
+    pass
+
+def subtract(a, b):
+    pass
+
+def multiply(a, b):
+    pass
+
+def divide(a, b):
+    pass
+
+def power(a, b):
+    pass
+
+def modulus(a, b):
+    pass
+
+def floor_division(a, b):
+    pass
+
+
+if __name__ == "__main__":
+    print("Calculator Application")
